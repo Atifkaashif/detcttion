7ed9543068e08c672a1028756471a833dad2073c
Atif Kashif <atifkaashif@gmail.com>
2026-04-28 15:58:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,33 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import tempfile
+import os
 
-st.title("Helmet Detection System")
-
+# Load model
 model = YOLO("best.pt")
 
-uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
+st.title("Helmet Detection App")
 
-if uploaded_file:
+uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image")
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    # Get file extension
+    file_ext = os.path.splitext(uploaded_file.name)[1]
+
+    # Create temp file with extension
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=file_ext)
+
+    # Save image correctly
     image.save(temp_file.name)
 
+    # Predict
     results = model(temp_file.name)
 
+    # Plot result
     result_img = results[0].plot()
 
-    st.image(result_img, caption="Detection Result")
+    st.image(result_img, caption="Detection Result", use_container_width=True)
